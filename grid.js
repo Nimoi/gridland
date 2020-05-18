c8ba6bc59c988ba2d6f3231331ddb681798e7c91
Nathaniel Cagle <ncagle@digital-strata.com>
2020-05-17 21:25:52 -0700

    Show 0,0 centered and use the offset to render part of the map

--- grid.js
+++ grid.js
@@ -8,30 +8,33 @@ let offset;
 let tilesWide;
 let tilesHigh;
 
-const tileSize = 24;
+const tileSize = 32;
 let tiles = [];
 let colors = [
-    '0084ff',
-    'ff3311',
-    '5511ee',
-    '33ee51',
-    'a4e129'
+    'red',
+    'blue',
+    'green',
+    'orange',
+    'black'
 ];
 
 function setupGrid (canvas) {
     tilesWide = Math.ceil(canvas.width / tileSize);
     tilesHigh = Math.ceil(canvas.height / tileSize);
-    for (let y = 0; y < tilesHigh; y++) {
+    for (let y = -100; y < tilesHigh; y++) {
         let row = [];
-        for (let x = 0; x < tilesWide; x++) {
+        for (let x = -100; x < tilesWide; x++) {
             row.push({
-                color: `#${colors[random.int(4)]}`,
+                x: x,
+                y: y,
+                color: `${colors[random.int(4)]}`,
                 hover: false,
                 selected: false
             });
         }
         tiles.push(row);
     }
+    setupOffset(canvas);
 }
 
 function setupOffset (canvas) {
@@ -43,24 +46,28 @@ function setupOffset (canvas) {
 
 function drawGrid (context) {
     loopTiles((tile, x, y) => {
-        let xOffset = x * tileSize;
-        let yOffset = y * tileSize;
+        let xOffset = tile.x * tileSize - offset.x;
+        let yOffset = tile.y * tileSize - offset.y;
         if (! tile) {
             return;
         }
         context.fillStyle = tile.color;
         context.fillRect(xOffset, yOffset, tileSize, tileSize);
         if (tile.hover) {
-            context.strokeStyle = '#fffefa';
+            context.strokeStyle = '#fff';
             context.strokeRect(xOffset, yOffset, tileSize, tileSize);
         }
+        context.font = '12px Arial';
+        context.fillStyle = 'white';
+        context.fillText(`${tile.x},${tile.y}`, xOffset, yOffset + 12);
     });
 }
 
 function getTileFromGrid (mousePos) {
-    let tileX = Math.floor(mousePos.x / tileSize);
-    let tileY = Math.floor(mousePos.y / tileSize);
-    return tiles[tileY][tileX];
+    let tileX = Math.floor((mousePos.x + offset.x) / tileSize);
+    let tileY = Math.floor((mousePos.y + offset.y) / tileSize);
+    let cornerTile = tiles[0][0];
+    return tiles[tileY - cornerTile.y][tileX - cornerTile.x];
 }
 
 function highlightTile (mousePos) {
